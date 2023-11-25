8d33cfd295622f8e57575e41f0020ce90e289832
GCBallesteros <dev+github@maxwellrules.com>
2023-11-26 00:56:25 +0100

    feat: Update banners

--- banner/make_gif.sh
+++ banner/make_gif.sh
@@ -7,9 +7,9 @@ python build_star_field.py
 echo 'Combining man, cube and stars'
 python blend_man_and_hypercube.py
 echo 'Converting to animated gif'
-convert  -delay 7 -loop 0 ./horizon_man_looking_hypercube/*.png ./horizon_man_looking_hypercube/banner.gif
+convert  -delay 7 -loop 0 ./horizon_man_looking_hypercube/*.png ./horizon_man_looking_hypercube/inverted_banner.gif
 echo 'Creating inverted version of gif'
-convert ./horizon_man_looking_hypercube/banner.gif -channel RGB -negate ./horizon_man_looking_hypercube/inverted_banner.gif
+convert ./horizon_man_looking_hypercube/banner.gif -channel RGB -negate ./horizon_man_looking_hypercube/banner.gif
 echo 'Optimizing gifs'
 mogrify -layers 'optimize' ./horizon_man_looking_hypercube/banner.gif
 mogrify -layers 'optimize' ./horizon_man_looking_hypercube/inverted_banner.gif
