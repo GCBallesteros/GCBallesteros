7e961616dd0b74b2adbbdb777152720ce891d1d5
GCBallesteros <dev+github@maxwellrules.com>
2023-11-26 19:06:44 +0100

    feat: Add shocks to the shaking via gaussian pulses

--- banner/frame_shaker.py
+++ banner/frame_shaker.py
@@ -1,18 +1,77 @@
 from pathlib import Path
+from collections import namedtuple
 
 import cv2
 import numpy as np
+from numpy.random import rand
+
+Pulse = namedtuple("Pulse", ["t0", "A", "sigma"])
 
 OUTPUT_FRAMES_FOLDER = Path("./shaken_day_frames")
 MAN_INPUT = Path("./dithered_man.png")
 CUBE_INPUT = Path("./hypercube_frames")
 
 SHAKED_CHANNELS = ["G", "B"]
-SHAKE_AMPLITUDE = 1.5
+SHAKE_AMPLITUDE = 0.05
+SCALE_NOISE = 0.003
+
+pulses = [
+    Pulse(45, 13, 8),
+    Pulse(15, 10, 3),
+]
 
 
 man = cv2.imread(str(MAN_INPUT))
 channel_mapping = {"R": 2, "G": 1, "B": 0}
+
+
+def compose_affines(t1, t2):
+    t1_extend = np.eye(3)
+    t2_extend = np.eye(3)
+
+    t1_extend[:2, :] = t1
+    t2_extend[:2, :] = t2
+
+    combined_t = t2_extend @ t1_extend
+
+    return combined_t[:2, :]
+
+
+def gaussian(t, t0, A, sigma):
+    return A * np.exp(-((t - t0) ** 2) / (2 * sigma * sigma))
+
+
+def shaking_amplitude_transform(t, pulses):
+    amplitude = sum([gaussian(t, pulse.t0, pulse.A, pulse.sigma) for pulse in pulses])
+
+    # scale_amp = np.random.rand() * 0.003 + 0.997
+    row_disp_amplitude = amplitude * (rand() - 0.5)
+    col_disp_amplitude = amplitude * (rand() - 0.5)
+
+    transform = np.array([[1, 0, row_disp_amplitude], [0, 1, col_disp_amplitude]])
+
+    return transform
+
+
+def shake_the_world(img, t, pulses, shaked_channels, shake_amplitude, scale_noise):
+    n_rows = img.shape[0]
+    n_cols = img.shape[1]
+    for ch in shaked_channels:
+        row_disp = (rand() - 0.5) * 2 * shake_amplitude
+        col_disp = (rand() - 0.5) * 2 * shake_amplitude
+        scale_amp = rand() * scale_noise + (1 - scale_noise)
+
+        # noise_transform = np.array([[1, 0, row_disp], [0, 1, col_disp]])
+        noise_transform = np.array([[scale_amp, 0, col_disp], [0, scale_amp, row_disp]])
+
+        img[:, :, channel_mapping[ch]] = cv2.warpAffine(
+            img[:, :, channel_mapping[ch]],
+            compose_affines(noise_transform, shaking_amplitude_transform(t, pulses)),
+            (n_cols, n_rows),
+            borderMode=cv2.BORDER_REFLECT_101,
+        )
+
+    return img
 
 
 for idx, input_cube_frame in enumerate(sorted(CUBE_INPUT.glob("frame_*.png"))):
@@ -38,21 +97,6 @@ for idx, input_cube_frame in enumerate(sorted(CUBE_INPUT.glob("frame_*.png"))):
     # NOTE: Finally trim a bit more from the top. Again strongly dependent on images
     man_ = man_[50:, :]
 
-    # At this point we want to add a little shake. We will do it via a displacement
-    # only affine transformation
-    n_rows = man_.shape[0]
-    n_cols = man_.shape[1]
-    for ch in SHAKED_CHANNELS:
-        row_disp = (np.random.rand() - 0.5) * 2 * SHAKE_AMPLITUDE
-        col_disp = (np.random.rand() - 0.5) * 2 * SHAKE_AMPLITUDE
-
-        noise_transform = np.array([[1, 0, row_disp], [0, 1, col_disp]])
-
-        man_[:, :, channel_mapping[ch]] = cv2.warpAffine(
-            man_[:, :, channel_mapping[ch]],
-            noise_transform,
-            (n_cols, n_rows),
-            borderMode=cv2.BORDER_REFLECT_101,
-        )
+    man_ = shake_the_world(man_, idx, pulses, SHAKED_CHANNELS, SHAKE_AMPLITUDE, SCALE_NOISE)
 
     cv2.imwrite(str(OUTPUT_FRAMES_FOLDER / f"frame_{idx:03}.png"), man_)
